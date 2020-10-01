e5682c265c4b9605f015e1b5b6f30fe83c8dec07
Plague26 <42643580+Plague26@users.noreply.github.com>
2020-10-01 10:56:24 +0530

    Update Main.cpp

--- Periodic Table/Main.cpp
+++ Periodic Table/Main.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<string.h>
-#include"Element.h"
+#include<iostream>                 //header file for input/output function
+#include<string.h>                //it is use to enter alphabatic string
+#include"Element.h"                // customized elements
 #include"Group.h">
 using namespace std;
 int main()
